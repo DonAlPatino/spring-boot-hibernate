71a2edda12115e304688c11827e5d8137da3652e
donalpatino <valp@inbox.ru>
2022-07-26 11:16:43 +0300

    Spring Boot with Hibernate example. spring.h2.console.enabled=true http://localhost:8080/

---
+++ src/test/java/com/springboothibernate/example/BookServiceUnitTest.java
@@ -0,0 +1,26 @@
+package com.springboothibernate.example;
+
+import com.springboothibernate.example.models.Book;
+import com.springboothibernate.example.services.BookService;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookServiceUnitTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void whenApplicationStarts_thenHibernateCreatesInitialRecords() {
+        List<Book> books = bookService.list();
+        Assert.assertEquals(books.size(), 3);
+    }
+}
